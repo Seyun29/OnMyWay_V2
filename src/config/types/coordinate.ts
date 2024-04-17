fe095da6bd5e80ff9fa8625165468a6204fb4cb9
SeyunJang <kj10522002@gmail.com>
2024-04-17 14:01:42 +0900

    update Marker related types

--- src/config/types/coordinate.ts
+++ src/config/types/coordinate.ts
@@ -2,3 +2,14 @@ export interface Coordinate {
   latitude: number;
   longitude: number;
 }
+
+export interface CoordDetail extends Coordinate {
+  //FIXME: add other properties here
+  open?: boolean;
+  category?: string; //FIXME: add enum for category
+}
+
+export interface OmWMarkerProps {
+  //TODO: add more props here
+  coordList: CoordDetail[];
+}
