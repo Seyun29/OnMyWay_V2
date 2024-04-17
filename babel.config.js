cc0d5633e5658084ab017f721ecd82914b25c903
SeyunJang <kj10522002@gmail.com>
2024-04-17 14:54:09 +0900

    update babel.config.js for .env

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+  plugins: [
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
