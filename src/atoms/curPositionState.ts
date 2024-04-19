236ae83730744714752d3b657ab4a17221f50fb6
SeyunJang <kj10522002@gmail.com>
2024-04-19 17:01:47 +0900

    update recoil state ('mapCenter'

--- src/atoms/curPositionState.ts
+++
@@ -1,8 +0,0 @@
-import {atom} from 'recoil';
-import {ANAM} from '../dummy/coord';
-import {Coordinate} from '../config/types/coordinate';
-
-export const curPositionState = atom<Coordinate>({
-  key: 'curPositionState',
-  default: ANAM,
-});
