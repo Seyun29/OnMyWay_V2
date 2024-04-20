9d8fd28b6f641216d6917975fe8a8a9a6b277d56
SeyunJang <kj10522002@gmail.com>
2024-04-20 15:22:52 +0900

    revert CurPosButton back to SVG

--- src/components/buttons/CurPosButton.tsx
+++ src/components/buttons/CurPosButton.tsx
@@ -8,13 +8,12 @@ export default function CurPosButton({onPress}: {onPress: () => void}) {
       className="absolute right-2.5 bottom-2.5"
       onPress={onPress}
       activeOpacity={0.2}>
-      {/* <CurPosButtonSVG height="50px" width="50px" /> 
-      FIXME: SVG not working properly, using PNG for now
-      */}
-      <Image
+      <CurPosButtonSVG height="50px" width="50px" />
+      {/* FIXME: Below - using PNG*/}
+      {/* <Image
         className="w-12 h-12"
         source={require('../../assets/images/curPosButton.png')}
-      />
+      /> */}
     </TouchableOpacity>
   );
 }
