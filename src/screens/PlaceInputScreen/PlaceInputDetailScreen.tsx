f38ed13f6ab674b07f437e15b03dfe16c1158e00
SeyunJang <kj10522002@gmail.com>
2024-04-24 00:53:18 +0900

    create PlaceInputDetailScreen.tsx

---
+++ src/screens/PlaceInputScreen/PlaceInputDetailScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function PlaceInputDetailScreen() {
+  return (
+    <View>
+      <Text>PlaceInputDetailScreen</Text>
+    </View>
+  );
+}
