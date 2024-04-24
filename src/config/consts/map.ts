8fad029be8051d86d118eafa4b3b6dbab56a4681
SeyunJang <kj10522002@gmail.com>
2024-04-24 20:26:33 +0900

    add const size type

---
+++ src/config/consts/map.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_ZOOM = 14;
+export const ENLARGE_ZOOM = 16;
+
+export const DEFAULT_MARKER_WIDTH = 21;
+export const DEFAULT_MARKER_HEIGHT = 27;
+export const LARGE_MARKER_WIDTH = 32;
+export const LARGE_MARKER_HEIGHT = 41;
+
+export const NAV_MARKER_WIDTH = 32;
+export const NAV_MARKER_HEIGHT = 32;
