e97d93d7279a35db420288c45bbaac8f16402f7e
SeyunJang <kj10522002@gmail.com>
2024-06-16 07:05:44 +0900

    getReviewSummary

--- src/config/consts/api.ts
+++ src/config/consts/api.ts
@@ -1,5 +1,4 @@
 import {SERVER_BASEURL} from '@env';
-//TODO: add api related consts here
 
 export const BASE_URL = SERVER_BASEURL;
 export const COORD_TO_ADDRESS = '/map/get-address';
@@ -10,3 +9,4 @@ export const GET_STOPBY_DURATION = '/map/stopby-duration';
 
 export const LOCAL_LLM_URL = '10.16.135.41'; //FIXME: change this to the local llm server url
 export const LLM_MODEL_NAME = 'gemma';
+export const GET_REVIEW_SUMMARY = '/map/get-review-summary';
