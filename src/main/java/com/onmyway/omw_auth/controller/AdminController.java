c676d944649f1d2b523d0a567e41859a40cf05d8
SeyunJang <kj10522002@gmail.com>
2024-07-24 15:28:59 +0900

    add basic controllers (draft)

---
+++ src/main/java/com/onmyway/omw_auth/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.onmyway.omw_auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class AdminController {
+}
