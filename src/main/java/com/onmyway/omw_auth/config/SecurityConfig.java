e5f53037fe351c0b57cf0ddc7de129576934129f
SeyunJang <kj10522002@gmail.com>
2024-07-24 16:27:04 +0900

    refactor to directly use method reference instead of lambda expression

--- src/main/java/com/onmyway/omw_auth/config/SecurityConfig.java
+++ src/main/java/com/onmyway/omw_auth/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,14 +21,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        //csrf disable
-        http.csrf((auth) -> auth.disable()); //session 방식에서는 session이 고정되기 때문에 csrf를 방어해야하나, JWT는 session이 stateless하기 때문에 csrf를 disable해줄 수 있다.
+        //session 방식에서는 session이 고정되기 때문에 csrf를 방어해야하나, JWT는 session이 stateless하기 때문에 csrf를 disable해줄 수 있다.
+        http.csrf(AbstractHttpConfigurer::disable);
 
         //Form 로그인 방식 disable
-        http.formLogin((auth) -> auth.disable());
+        http.formLogin(AbstractHttpConfigurer::disable);
 
         //http basic 인증 방식 disable
-        http.httpBasic((auth) -> auth.disable());
+        http.httpBasic(AbstractHttpConfigurer::disable);
 
         //경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth.requestMatchers("/login", "/", "/join")
