755542dd032e50ea3e5ba73359851a3395e0c1f3
SeyunJang <kj10522002@gmail.com>
2024-07-24 15:01:52 +0900

    Initialize SpringBoot

---
+++ src/test/java/com/onmyway/omw_auth/OmwAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onmyway.omw_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmwAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
